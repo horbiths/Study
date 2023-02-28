6457e358b74cbd34c9c9c889ea1612aeb0a1387d
horbiths <gkrtjs77@gmail.com>
2023-02-28 15:51:57 +0900

    commit

---
+++ git-java/.metadata/.plugins/org.eclipse.core.resources/.history/89/30c4263187b5001d13b0c40fb2a8ab15
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author ORBI
+ *
+ */
+module Chapter1 {
+}
---
+++ git-java/Chapter1/src/ch09/BooleanTest.java
@@ -0,0 +1,11 @@
+package ch09;
+
+public class BooleanTest {
+
+	public static void main(String[] args) {
+		boolean imman = true;
+		System.out.println(imman);
+
+	}
+
+}
